f5c1b9adccda6ebd9bd352796a6ea917d4e9dd40
Stardream <me@stardream.online>
2024-05-12 20:03:31 +1000

    Site updated: 2024-05-12 20:03:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/2022Japan/","posts/2022/","posts/K-ON!/","posts/undefined/","posts/aad5/","posts/204/","posts/5a9/","posts/f07b/","posts/4301/","posts/d71a/","posts/d401/","posts/f51d/","posts/a70f/","posts/b700/","posts/1abe/","posts/28c8/","posts/AMAD/","posts/aedd/","posts/270c/","posts/safego/"];function toRandomPost(){
+var posts=["posts/2022Japan/","posts/2022/","posts/K-ON!/","posts/aad5/","posts/undefined/","posts/5a9/","posts/204/","posts/f07b/","posts/d71a/","posts/4301/","posts/d401/","posts/f51d/","posts/1abe/","posts/a70f/","posts/b700/","posts/28c8/","posts/AMAD/","posts/aedd/","posts/safego/","posts/270c/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
