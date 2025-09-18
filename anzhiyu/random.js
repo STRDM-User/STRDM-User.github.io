0d5b07dd0261666816cbfb62237b5f638e27e310
Stardream <me@stardream.online>
2025-09-19 04:54:41 +1000

    Site updated: 2025-09-19 04:54:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/2022Japan/","posts/2022/","posts/K-ON!/","posts/204/","posts/5a9/","posts/aad5/","posts/d71a/","posts/undefined/","posts/d401/","posts/f07b/","posts/4301/","posts/1abe/","posts/f51d/","posts/a70f/","posts/28c8/","posts/safego/","posts/b700/","posts/aedd/","posts/bd-key-decrypt/","posts/270c/","posts/Stagecrowd-DRM/","posts/AMAD/","posts/discord-presence-roon/"];function toRandomPost(){
+var posts=["posts/2022Japan/","posts/2022/","posts/K-ON!/","posts/undefined/","posts/f07b/","posts/5a9/","posts/d71a/","posts/4301/","posts/aad5/","posts/f51d/","posts/1abe/","posts/d401/","posts/a70f/","posts/28c8/","posts/b700/","posts/204/","posts/discord-presence-roon/","posts/safego/","posts/bd-key-decrypt/","posts/aedd/","posts/AMAD/","posts/Stagecrowd-DRM/","posts/270c/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
